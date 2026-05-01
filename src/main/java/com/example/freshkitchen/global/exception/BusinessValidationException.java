9f597573bb66c7292f69bad0f7261961a55850f3
JEON HAN JOON <hjun230604@gmail.com>
2026-05-01 21:03:46 +0900

    수정 완료

--- src/main/java/com/example/freshkitchen/global/exception/BusinessValidationException.java
+++
@@ -1,8 +0,0 @@
-//package com.example.freshkitchen.global.exception;
-//
-//public class BusinessValidationException extends BusinessException {
-//
-//    public BusinessValidationException(String detailMessage) {
-//        super(ErrorCode.INVALID_INPUT, detailMessage);
-//    }
-//}
